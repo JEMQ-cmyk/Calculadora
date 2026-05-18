73de19c8e2599348f6ac668f3967ff8fcd9092a3
João Eudes Marques <joaoeudes.eudesmar@gmail.com>
2026-05-18 14:33:28 -0300

    update archives

---
+++ teste/teste/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace teste
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private bool clearText = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Event_Click(object sender, RoutedEventArgs e)
+        {
+            operações operation = new operações();
+
+            Button btn = (Button)sender;
+
+            switch (btn.Name)
+            {
+
+                case "btn0":
+
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+
+                    textarea.Text += "0";
+                    break;
+                case "btn1":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "1";
+                    break;
+                case "btn2":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "2";
+                    break;
+                case "btn3":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "3";
+                    break;
+                case "btn4":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "4";
+                    break;
+                case "btn5":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "5";
+                    break;
+                case "btn6":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "6";
+                    break;
+                case "btn7":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "7";
+                    break;
+                case "btn8":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "8";
+                    break;
+                case "btn9":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "9";
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    break;
+                case "btnSoma":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "+";
+                    break;
+                case "btnSub":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "-";
+                    break;
+                case "btnMult":
+                    if (clearText)
+                    {
+                        textarea.Clear();
+                        clearText = false;
+                    }
+                    textarea.Text += "x";
+                    break;
+                case "btnIgual":
+                    string? operação = textarea.Text;
+                    textarea.Clear();
+                    textarea.Text = "=" + operation.operations(operação);
+                    clearText = true;
+                    break;
+
+
+            }
+            ;
+
+
+        }
+    }
+}
---
+++ teste/teste/operações.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste
+{
+    class operações
+    {
+
+        public double operations (string operações)
+        {
+
+            double result = 0;
+
+            if (operações.Contains("+"))
+            {
+                string[] numbers = operações.Split("+");
+
+                if( numbers.Length == 2)
+                {
+
+                    if (double.TryParse(numbers[0], out double number1) && double.TryParse(numbers[1], out double number2))
+                    {
+
+                        result = number1 + number2;
+
+                    }
+
+                }
+            }
+            else if (operações.Contains("-"))
+            {
+                string[] numbers = operações.Split("-");
+
+                if (numbers.Length == 2)
+                {
+
+                    if (double.TryParse(numbers[0], out double number1) && double.TryParse(numbers[1], out double number2))
+                    {
+
+                        result = number1 - number2;
+
+                    }
+
+                }
+            }
+            else if (operações.Contains("x"))
+            {
+                string[] numbers = operações.Split("x");
+
+                if (numbers.Length == 2)
+                {
+
+                    if (double.TryParse(numbers[0], out double number1) && double.TryParse(numbers[1], out double number2))
+                    {
+
+                        result = number1 * number2;
+
+                    }
+
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
